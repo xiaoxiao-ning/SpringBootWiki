66a274ae83b955e52758731bda05737b4a5c94da
小小宁 <2428879146@qq.com>
2021-03-21 20:49:40 +0800

    将初始化Vue项目时的HelloVue组件给删掉了

---
+++ web/src/components/theFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    小小宁的Wiki项目aaaaa
+  </a-layout-footer>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'theFooter'
+});
+</script>
+<style scoped>
+
+</style>
