f0ccd56f451aa58bce86c711017ba566ee68c24f
小小宁 <2428879146@qq.com>
2021-03-21 19:48:16 +0800

    继承Antd-vue的ui框架，并成功引入组件

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
 //看到#app就想到了public目录下index.html中页面的id
 //在实现逻辑上，vue是通过执行main.ts将内容页App.Vue中的内容渲染到index.html上
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(Antd).mount('#app')
