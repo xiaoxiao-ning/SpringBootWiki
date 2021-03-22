114bd6a6ee40ec8128aac5787b24b725a224cf98
小小宁 <2428879146@qq.com>
2021-03-22 19:16:43 +0800

    导入antd-vue中icon组件库

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,7 +4,15 @@ import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+import * as Icons from '@ant-design/icons-vue';
 
 //看到#app就想到了public目录下index.html中页面的id
 //在实现逻辑上，vue是通过执行main.ts将内容页App.Vue中的内容渲染到index.html上
-createApp(App).use(store).use(router).use(Antd).mount('#app')
+const app = createApp(App);
+app.use(store).use(router).use(Antd).mount('#app');
+
+
+const icons : any = Icons;
+for(const i in icons){
+    app.component(i,icons[i]);
+}
